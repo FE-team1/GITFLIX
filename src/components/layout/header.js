610b837787b9489d06f98a9356a26a8c80e91acd
kiminn <808010x@gmail.com>
2023-09-28 17:29:06 +0900

    feat: outlet라우터 설정

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { flexAlignCenter } from '../../styles/common.style';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
     const [searchBox, setSearchBox] = useState(false);
@@ -21,6 +22,7 @@ const Header = () => {
     };
     
     return (
+        <>
         <S.Wrapper >
             <S.Container>
                 <S.Logo src="img/githubicon.png"></S.Logo>
@@ -46,6 +48,8 @@ const Header = () => {
                 )}
             </S.Container>
         </S.Wrapper>
+        <Outlet/>
+    </>
     );
 };
 
