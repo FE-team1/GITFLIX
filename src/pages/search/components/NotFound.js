ec973a338e6b42062dca57dc68b64ca6b8fb2f3a
HeesikK <7jjhkhz@naver.com>
2023-10-04 21:47:08 +0900

    feat: not found 페이지 & favicon 변경 완료

--- src/pages/search/components/NotFound.js
+++ src/pages/search/components/NotFound.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import { flexCenter } from "../../../styles/common.style";
-import { Typography } from "@mui/material";
+import { useSearchParams } from "react-router-dom";
 
 const NotFound = () => {
+  const [query, setQuery] = useSearchParams();
+  let keyword = query.get("q");
   return (
     <Wrapper>
-      <Typography variant="h3" gutterBottom color={"gray"}>
-        Movie Not Found😥
-      </Typography>
+      <ErrorTitle>There are no results matching the search term "{keyword}" you entered.</ErrorTitle>
     </Wrapper>
   );
 };
@@ -16,6 +16,12 @@ export default NotFound;
 
 const Wrapper = styled.div`
   width: 80%;
-  height: 100vh;
+  height: 80vh;
   ${flexCenter};
+`;
+
+const ErrorTitle = styled.h2`
+  font-size: 30px;
+  margin-right: 15px;
+  color: gray;
 `;
